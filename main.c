6dea672973af8052f6a8d7d2d87b27f792a53496
yoojinlee-ai <yoojinlee@sookmyung.ac.kr>
2025-10-16 15:58:38 +0900

    7-5

--- main.c
+++ main.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
+void sub(void);
 
-void f(void);
-
-int main(void) {
+int main(void)
+{
     int i;
-    for (i = 0; i < 5; i++) {
-        f();
-        printf("\n"); 
-    }
-    
-    system("PAUSE");
+    for (i = 0; i < 3; i++)
+        sub();
     return 0;
 }
 
-void f(void) {
-    int i;
-    for (i = 0; i < 10; i++) {
-        printf("#");
-    }
+void sub(void)
+{
+    int auto_count = 0;
+    static int static_count = 0;
+    auto_count++;
+    static_count++;
+    printf("auto_count=%d\n", auto_count);
+    printf("static_count=%d\n", static_count);
 }
+
 
