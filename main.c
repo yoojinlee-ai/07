a8d50753934a6b87271f31cb7bd1d73f4bbe90a5
yoojinlee-ai <yoojinlee@sookmyung.ac.kr>
2025-10-16 15:37:05 +0900

    7-3

--- main.c
+++ main.c
@@ -3,18 +3,20 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
-	int i; 
-	int temp;
-	for (i=0;i<5;i++) 
-	{ 
-	static int temp=1; 
-	printf("temp = %d\n", temp); 
-	temp++; 
-	} 
-	
-	system("PAUSE");
-	
-	
-	return 0;
+int inc(int counter); 
+int main(int argc, char*argv[]) 
+{ 
+  int i=10; 
+  printf("함수 호출전 i=%d\n", i); 
+  i = inc(i); 
+  printf("함수 호출후 i=%d\n", i); 
+  system("PAUSE");
+  return 0; 
+} 
+int inc(int counter) 
+{ 
+    counter++; 
+	return counter;
+  
+  
 }
